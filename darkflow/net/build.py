ee8778e2accf498e548f370ff28892895df36483
Vinicius Trevisan <vinicius_trev@hotmail.com>
2019-05-06 22:53:02 -0300

    Organizacao

--- darkflow/net/build.py
+++ darkflow/net/build.py
@@ -130,7 +130,7 @@ class TFNet(object):
 		if utility > 0.0:
 			self.say('GPU mode with {} usage'.format(utility))
 			cfg['gpu_options'] = tf.GPUOptions(
-				per_process_gpu_memory_fraction = utility)
+				per_process_gpu_memory_fraction = utility, allow_growth = True)
 			cfg['allow_soft_placement'] = True
 		else: 
 			self.say('Running entirely on CPU')
